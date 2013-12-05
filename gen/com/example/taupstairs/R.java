613a85c34c713854d6b6c550033f7e641f657c7b
hongdong <fjhongdong@126.com>
2013-12-05 14:45:33 +0800

    Signed-off-by: hongdong <fjhongdong@126.com>

--- gen/com/example/taupstairs/R.java
+++ gen/com/example/taupstairs/R.java
@@ -465,15 +465,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int img_select=0x7f020001;
-        public static final int login_bg=0x7f020002;
-        public static final int login_bg_bn=0x7f020003;
-        public static final int login_bg_list=0x7f020004;
-        public static final int login_bg_txt=0x7f020005;
-        public static final int logo=0x7f020006;
-        public static final int logo_bg=0x7f020007;
-        public static final int taupstairs=0x7f020008;
+        public static final int content=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int img_select=0x7f020002;
+        public static final int login_bg=0x7f020003;
+        public static final int login_bg_bn=0x7f020004;
+        public static final int login_bg_list=0x7f020005;
+        public static final int login_bg_txt=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int logo_bg=0x7f020008;
+        public static final int taupstairs=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f0b0005;
