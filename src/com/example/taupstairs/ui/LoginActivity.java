dde0074934bf2ad9e90cad9335b1cc76213571a7
baiwgi <296216078@qq.com>
2013-12-14 17:31:33 +0800

    进度条加了，选择学校也一整行都变灰了
    Signed-off-by: baiwgi <296216078@qq.com>

--- src/com/example/taupstairs/ui/LoginActivity.java
+++ src/com/example/taupstairs/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.taupstairs.ui;
 import java.util.HashMap;
 import java.util.Map;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ public class LoginActivity extends Activity implements ItaActivity {
 	private User user;
 	private Button bn_login;
 	private String userCollegeId;
+	private ProgressDialog progressDialog;
 	private TextView txt_college_name, txt_about, txt_server;
 	private EditText edit_studentid, edit_password;
 	@Override
@@ -36,13 +38,14 @@ public class LoginActivity extends Activity implements ItaActivity {
 	}
 
 	public void init() {
-		txt_college_name = (TextView)findViewById(R.id.txt_college_name);
-		txt_college_name.setOnClickListener(new OnClickListener() {
+		View view = findViewById(R.id.layout_login_college);
+		view.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent(LoginActivity.this, SelectCollegeActivity.class);
 				startActivityForResult(intent, IntentInfo.RequestCode.LOGIN_SELECTCOLLEGE);
 			}
 		});
+		txt_college_name = (TextView)findViewById(R.id.txt_college_name);
 		edit_studentid = (EditText)findViewById(R.id.edit_studentid);
 		edit_password = (EditText)findViewById(R.id.edit_password);
 		bn_login = (Button)findViewById(R.id.bn_login);
@@ -78,15 +81,20 @@ public class LoginActivity extends Activity implements ItaActivity {
 		});
 	}
 	
-	/*登录的后台任务处理*/
+	/*登录放到后台处理*/
 	private void loginTask(User user) {
+		progressDialog = new ProgressDialog(LoginActivity.this);
+		progressDialog.setCancelable(false);
+		progressDialog.setMessage("正在登录...");
+		progressDialog.show();
 		Map<String, Object> taskParams = new HashMap<String, Object>();
 		taskParams.put(Task.TA_LOGIN_TASKPARAMS, user);
 		Task task = new Task(Task.TA_LOGIN, taskParams);
 		MainService.addTask(task);
 	}
 
-	public void refresh(Object... params) {					
+	public void refresh(Object... params) {				
+		progressDialog.dismiss();
 		String result = ((String) params[0]).trim();	//这里的字符串要去空格，不然很可能不会equals
 		if (result.equals(Task.TA_LOGIN_ERROR)) {
 			loginError();
@@ -102,17 +110,17 @@ public class LoginActivity extends Activity implements ItaActivity {
 	/*未连接网络或服务器响应异常*/
 	private void loginError() {
 		Toast.makeText(LoginActivity.this, 
-				"未连接网络或服务器响应异常", 
+				"未连接网络", 
 				Toast.LENGTH_SHORT).show();
 	}
 	
 	/*登录信息错误，登录失败*/
 	private void loginFalse() {
 //		txt_college_name.setText("点击选择");
-		edit_studentid.setText("");
+//		edit_studentid.setText("");
 		edit_password.setText("");
 		Toast.makeText(LoginActivity.this, 
-				"用户信息错误，请从新选择学校并填写学号和密码", 
+				"    用户信息错误\n请从新选择与填写", 
 				Toast.LENGTH_SHORT).show();
 	}
 	
